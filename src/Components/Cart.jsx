b12b2ea143069fd4f5408b16464cb6fc9c290c90
Milos Petrovic <milos.petrovic09.95@gmail.com>
2025-02-13 23:23:00 +0100

    creating cart

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,14 @@
+const Cart = () => {
+  return (
+    <div className="div-main">
+      <div className="cart-headline"></div>
+      <div className="cart-item"></div>
+      <div className="cart-buttons">
+        <button>Order pizza</button>
+        <button>Clear Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
